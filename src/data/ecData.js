1e77ecddf49b8d4ef0de0ac6ae8b461c22efbacd
Nathan-47 <nathan.parchment47@gmail.com>
2025-10-14 16:49:26 +0100

    Add ecData.js file with questions and links for Extenuating Circumstances process

---
+++ src/data/ecData.js
@@ -0,0 +1,46 @@
+const ecData = [
+    {
+        question: "Do you need...",
+        id: 0,
+        answerA: "An Extension of 10 working days (Nb an extension is only for coursework type assessments)?",
+        idNextQuestionA: 1,
+        answerB: "A Deferral (do at next opportunity instead of this one)?",
+        idNextQuestionB: 2
+    },
+    {
+        question: "Have you submitted a Self-Certification EC claim already this semester?",
+        id: 1,
+        answerA: "Yes",
+        idNextQuestionA: 3,
+        answerB: "No",
+        idNextQuestionB: 4
+    },
+    {
+        question: "Follow the Full EC claim process",
+        id: 2,
+        resultA: "https://www.bcusu.com/advice/extenuatingcircumstances/full/"
+    },
+    {
+        question: "You will need to claim for a Full Extenuating Circumstances extension",
+        id: 3,
+        resultA: "https://www.bcusu.com/advice/extenuatingcircumstances/full/"
+    },
+    {
+        question: "Can you easily access independent evidence of your circumstances?",
+        id: 4,
+        answerA: "Yes",
+        idNextQuestionA: 5,
+        answerB: "No",
+        idNextQuestionB: 6
+    },
+    {
+        question: "Choose a Full EC extension if you feel you may need a Self-Certification extension later in the same semester",
+        id: 5,
+        resultA: "https://www.bcusu.com/advice/extenuatingcircumstances/full/"
+    },
+    {
+        question: "It is advisable to choose a Self-Certification EC claim as this does not require evidence",
+        id: 6,
+        resultA: "https://www.bcusu.com/advice/extenuatingcircumstances/selfcert/"
+    },
+]
