8837c8745ae9b4b24670b60ce71e962ac98de3ab
Nathan-47 <nathan.parchment47@gmail.com>
2025-10-30 12:58:23 +0000

    Updated questions and added button link text to make it easier for users to flow through quiz

--- src/data/ecData.js
+++ src/data/ecData.js
@@ -4,11 +4,11 @@ const ecData = [
         id: 0,
         answerA: "An Extension of 10 working days (Nb an extension is only for coursework type assessments)?",
         idNextQuestionA: 1,
-        answerB: "A Deferral (do at next opportunity instead of this one)?",
+        answerB: "A Deferral (i.e. defer to next opportunity or get attempt back)",
         idNextQuestionB: 2
     },
     {
-        question: "Have you submitted a Self-Certification EC claim already this semester?",
+        question: "Have you already made a Self-Certification EC claim this semester?",
         id: 1,
         answerA: "Yes",
         idNextQuestionA: 3,
@@ -16,14 +16,16 @@ const ecData = [
         idNextQuestionB: 4
     },
     {
-        question: "Follow the Full EC claim process",
+        question: "To claim a Deferral you need to follow the Full EC claim process (click below for more info):",
         id: 2,
-        resultA: "https://www.bcusu.com/advice/extenuatingcircumstances/full/"
+        resultA: "https://www.bcusu.com/advice/extenuatingcircumstances/full/",
+        linkText: "Click for Full EC Claim info"
     },
     {
-        question: "You will need to claim for a Full Extenuating Circumstances extension",
+        question: "Because you have already made a claim you will need to claim for a Full Extenuating Circumstances extension (click below for more info):",
         id: 3,
-        resultA: "https://www.bcusu.com/advice/extenuatingcircumstances/full/"
+        resultA: "https://www.bcusu.com/advice/extenuatingcircumstances/full/",
+        linkText: "Click for Full EC Claim info"
     },
     {
         question: "Can you easily access independent evidence of your circumstances?",
@@ -34,13 +36,17 @@ const ecData = [
         idNextQuestionB: 6
     },
     {
-        question: "Choose a Full EC extension if you feel you may need a Self-Certification extension later in the same semester",
+        question: "Choose a Full EC extension if you feel you may need a Self-Certification extension later in the same semester. If you don’t, choose a Self-Certification EC claim as this is quicker and easier.",
         id: 5,
-        resultA: "https://www.bcusu.com/advice/extenuatingcircumstances/full/"
+        resultA: "https://www.bcusu.com/advice/extenuatingcircumstances/full/",
+        resultB: "https://www.bcusu.com/advice/extenuatingcircumstances/selfcert/",
+        linkText: "Click for Full EC Claim info",
+        linkTextB: "Click for Self-Certification EC Claim info"
     },
     {
         question: "It is advisable to choose a Self-Certification EC claim as this does not require evidence",
         id: 6,
-        resultA: "https://www.bcusu.com/advice/extenuatingcircumstances/selfcert/"
+        resultA: "https://www.bcusu.com/advice/extenuatingcircumstances/selfcert/",
+        linkText: "Click for Self-Certification EC Claim info"
     },
 ]
